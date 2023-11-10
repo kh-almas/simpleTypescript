349eb80755bfc650de596ed654432e035cde489e
Md Almas Hossen <services.almas@gmail.com>
2023-11-10 21:41:30 +0600

    solve promise problem

--- module2/src/typesscriptPromise.ts
+++ module2/src/typesscriptPromise.ts
@@ -25,7 +25,7 @@
             const data = await response.json();
             console.log(data);
         } catch (error) {
-            console.error('Error fetching data:', error.message);
+            console.error('Error fetching data:', error);
         }
     };
 
